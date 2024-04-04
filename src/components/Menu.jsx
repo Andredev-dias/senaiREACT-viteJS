b93973b8388b7bf58f48987fd0522d0d1965843a
Andre Dias <andredias.curitiba@gmail.com>
2024-04-03 22:57:44 -0300

    vercel try 2

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,7 @@
 export default function Menu(){
   return (
     <ul>
-      <li><a href={`/`}>Graficos Recharts</a> </li>
+      <li><a href={`/`}>Contas</a> </li>
       <li><a href={`/consume`}>JSON Consumo</a> </li>
       <li><a href={`/rm`}>Rick and Morty Consumo</a> </li>
     </ul>
